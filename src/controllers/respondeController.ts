038d3f49b328adc60a5c20cfd7a2dca683f954c4
YURI FERREIRA <yurimoreiraferreira@hotmail.com>
2024-08-15 18:47:54 -0300

    arrumando id_usuario

--- src/controllers/respondeController.ts
+++ src/controllers/respondeController.ts
@@ -1,27 +1,57 @@
-import { FastifyReply, FastifyRequest } from "fastify";
+import { FastifyRequest, FastifyReply } from "fastify";
 import db from "../db/connection";
 
-interface RespostaBody {
-  fk_Usuario_id_usuario: number;
-  fk_Questao_id_questao: number;
-  resposta: string;
-}
-
-export const saveResposta = async (
-  request: FastifyRequest<{ Body: RespostaBody }>,
+export const respondeController = async (
+  request: FastifyRequest<{
+    Body: {
+      fk_Usuario_id_usuario: number;
+      fk_Questao_id_questao: number;
+      respostas_abertas: string;
+      respostas_fechadas: number; // Ajustado para refletir o tipo tinyint
+      datahora: string; // Ajustado para refletir o tipo date
+      resposta: string;
+    };
+  }>,
   reply: FastifyReply
 ) => {
   try {
-    const { fk_Usuario_id_usuario, fk_Questao_id_questao, resposta } = request.body;
-    await db('Responde').insert({
+    console.log("Recebendo dados do frontend:", request.body);
+
+    const {
       fk_Usuario_id_usuario,
       fk_Questao_id_questao,
+      respostas_abertas,
+      respostas_fechadas,
+      datahora,
       resposta,
-      datahora: new Date(),
+    } = request.body;
+
+    console.log(`fk_Usuario_id_usuario: ${fk_Usuario_id_usuario}`);
+    console.log(`fk_Questao_id_questao: ${fk_Questao_id_questao}`);
+    console.log(`Respostas Abertas: ${respostas_abertas}`);
+    console.log(`Respostas Fechadas: ${respostas_fechadas}`);
+    console.log(`Data/Hora: ${datahora}`);
+    console.log(`Resposta: ${resposta}`);
+    console.log("Tentando inserir dados na tabela Responde...");
+
+    await db("Responde").insert({
+      fk_Usuario_id_usuario,
+      fk_Questao_id_questao,
+      respostas_abertas,
+      respostas_fechadas,
+      datahora,
+      resposta,
     });
-    reply.send({ success: true });
+
+    console.log("Dados inseridos com sucesso na tabela Responde");
+    return reply.send({ message: "Resposta inserida com sucesso" });
   } catch (error) {
-    console.error("Erro ao salvar resposta:", error);
-    reply.status(500).send("Erro ao salvar resposta!");
+    console.error("Erro ao inserir resposta:", error.message || error);
+    return reply
+      .status(500)
+      .send({
+        error: "Erro ao inserir resposta",
+        details: error.message || error,
+      });
   }
 };
