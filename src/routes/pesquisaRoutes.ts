06f80d9f826c70346819cc6981e8542428541bd0
YuriFerreira0 <yurimoreiraferreira@hotmail.com>
2024-10-06 15:45:54 -0300

    Alteracoes Finais no app

--- src/routes/pesquisaRoutes.ts
+++ src/routes/pesquisaRoutes.ts
@@ -1,9 +1,16 @@
-import { FastifyInstance } from 'fastify';
-import { createPesquisa, searchPesquisa } from '../controllers/pesquisaController';
+import { FastifyInstance } from "fastify";
+import {
+  createPesquisa,
+  searchPesquisa,
+  getAllPesquisas,
+  getUsuariosByPesquisa,
+} from "../controllers/pesquisaController";
 
 const pesquisaRoutes = async (server: FastifyInstance) => {
-  server.post('/Pesquisa', createPesquisa);
-  server.get('/search-pesquisa', searchPesquisa);
+  server.post("/Pesquisa", createPesquisa);
+  server.get("/search-pesquisa", searchPesquisa);
+  server.get("/PesquisasAll", getAllPesquisas); // Para retornar todas as pesquisas
+  server.get("/usuarios-pesquisa/:nome_pesq", getUsuariosByPesquisa);
 };
 
 export default pesquisaRoutes;
--- src/routes/respostasDuracaoFrequenciaClassificacaoRoutes.ts
+++ src/routes/respostasDuracaoFrequenciaClassificacaoRoutes.ts
@@ -1,32 +1,43 @@
 import { FastifyInstance } from "fastify";
 import {
-    resp_caminhada_duracao_total,
-    resp_caminhada_frequencia_total,
-    resp_moderada_duracao_total,
-    resp_frequencia_moderada_total,
-    resp_vigorosa_duracao_total,
-    resp_frequencia_vigorosa_total,
-    resp_caminhada_duracao_media,
-    resp_duracao_media_moderada,
-    resp_duracao_media_vigorosa,
-    resp_freq_mod_cam,
-    resp_dur_mod_cam,
-    classificacao
+  resp_caminhada_duracao_total,
+  resp_caminhada_frequencia_total,
+  resp_moderada_duracao_total,
+  resp_frequencia_moderada_total,
+  resp_vigorosa_duracao_total,
+  resp_frequencia_vigorosa_total,
+  resp_caminhada_duracao_media,
+  resp_duracao_media_moderada,
+  resp_duracao_media_vigorosa,
+  resp_freq_mod_cam,
+  resp_dur_mod_cam,
+  classificacao,
 } from "../controllers/respostasDuracaoFrequenciaClassificacaoController";
 
-const respostasDuracaoFrequenciaClassificacaoRoutes = async (server: FastifyInstance) => {
-    server.get("/respostaCaminhadaDuracaoTotal", resp_caminhada_duracao_total);
-    server.get("/respostaCaminhadaFrequenciaTotal", resp_caminhada_frequencia_total);
-    server.get("/respostaModeradaDuracaoTotal", resp_moderada_duracao_total);
-    server.get("/respostaModeradaFrequenciaTotal", resp_frequencia_moderada_total);
-    server.get("/respostaVigorosaDuracaoTotal", resp_vigorosa_duracao_total);
-    server.get("/respostaVigorosaFrequenciaTotal", resp_frequencia_vigorosa_total);
-    server.get("/respostaDuracaoMedia",  resp_caminhada_duracao_media);
-    server.get("/respostaDuracaoModerada", resp_duracao_media_moderada);
-    server.get("/respostaDuracaoVigorosa", resp_duracao_media_vigorosa);
-    server.get("/respostaFreqModCam", resp_freq_mod_cam);
-    server.get("/respostaDurModCam", resp_dur_mod_cam);
-    server.get("/classificacao", classificacao);
+const respostasDuracaoFrequenciaClassificacaoRoutes = async (
+  server: FastifyInstance
+) => {
+  server.get("/respostaCaminhadaDuracaoTotal", resp_caminhada_duracao_total);
+  server.get(
+    "/respostaCaminhadaFrequenciaTotal",
+    resp_caminhada_frequencia_total
+  );
+  server.get("/respostaModeradaDuracaoTotal", resp_moderada_duracao_total);
+  server.get(
+    "/respostaModeradaFrequenciaTotal",
+    resp_frequencia_moderada_total
+  );
+  server.get("/respostaVigorosaDuracaoTotal", resp_vigorosa_duracao_total);
+  server.get(
+    "/respostaVigorosaFrequenciaTotal",
+    resp_frequencia_vigorosa_total
+  );
+  server.get("/respostaDuracaoMedia", resp_caminhada_duracao_media);
+  server.get("/respostaDuracaoModerada", resp_duracao_media_moderada);
+  server.get("/respostaDuracaoVigorosa", resp_duracao_media_vigorosa);
+  server.get("/respostaFreqModCam", resp_freq_mod_cam);
+  server.get("/respostaDurModCam", resp_dur_mod_cam);
+  server.get("/classificacao", classificacao);
 };
 
 export default respostasDuracaoFrequenciaClassificacaoRoutes;
